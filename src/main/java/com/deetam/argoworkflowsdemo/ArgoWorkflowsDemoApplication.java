e4f225eec710e4fc1f1ea24a9642e717d7f33740
unknown <unknown@unknown.com>
2024-03-21 21:30:09 +0800

    Initial commit

---
+++ src/main/java/com/deetam/argoworkflowsdemo/ArgoWorkflowsDemoApplication.java
@@ -0,0 +1,22 @@
+package com.deetam.argoworkflowsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ArgoWorkflowsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgoWorkflowsDemoApplication.class, args);
+	}
+
+	@RestController
+	static class TestController {
+		@GetMapping("/hi")
+		public String test() {
+			return "Hello World";
+		}
+	}
+}
---
+++ src/test/java/com/deetam/argoworkflowsdemo/ArgoWorkflowsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deetam.argoworkflowsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgoWorkflowsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
