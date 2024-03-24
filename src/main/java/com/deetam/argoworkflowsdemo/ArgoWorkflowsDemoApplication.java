1cf162dde782b00b623ea9202248e6beb8010fb9
unknown <unknown@unknown.com>
2024-03-25 01:37:10 +0800

    update version

--- src/main/java/com/deetam/argoworkflowsdemo/ArgoWorkflowsDemoApplication.java
+++ src/main/java/com/deetam/argoworkflowsdemo/ArgoWorkflowsDemoApplication.java
@@ -16,7 +16,7 @@ public class ArgoWorkflowsDemoApplication {
 	static class TestController {
 		@GetMapping("/hi")
 		public String test() {
-			return "Hello World --- v4";
+			return "Hello World --- v5";
 		}
 	}
 }
